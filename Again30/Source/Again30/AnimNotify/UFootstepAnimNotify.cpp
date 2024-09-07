79442969bb9cb808ab20a1224df53d3e29a33ba3
husk <huskgit@gmail.com>
2024-09-07 21:46:49 +0900

    물고기는 사운드가 있고 사운드는 저작권 때문에 리소스는 못올리는 상황

---
+++ Again30/Source/Again30/AnimNotify/UFootstepAnimNotify.cpp
@@ -0,0 +1,29 @@
+﻿#include "UFootstepAnimNotify.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
+
+UUFootstepAnimNotify::UUFootstepAnimNotify()
+	:
+	CheckDistance(20.f)
+{
+}
+
+void UUFootstepAnimNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                  const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	if(MeshComp == nullptr || MeshComp->GetWorld() == nullptr)	return;
+
+	FHitResult HitResult;
+	const FVector FootLocation = MeshComp->GetSocketLocation(BoneName);
+	const FVector End{ FootLocation + FVector{0.f, 0.f, -100.f } };
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(MeshComp->GetOwner());
+
+	auto World = MeshComp->GetWorld();
+	World->LineTraceSingleByChannel(HitResult, FootLocation, End, ECC_WorldStatic, QueryParams);
+	if(HitResult.bBlockingHit && HitResult.Distance < CheckDistance)
+	{
+		UGameplayStatics::PlaySoundAtLocation(MeshComp->GetWorld(), FootStepSound, FootLocation, FRotator::ZeroRotator, VolumeMultiplier);
+	}
+}
---
+++ Again30/Source/Again30/AnimNotify/UFootstepAnimNotify.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "UFootstepAnimNotify.generated.h"
+
+class USoundCue;
+struct FAnimNotifyEventReference;
+class UAnimSequenceBase;
+class USkeletalMeshComponent;
+
+UCLASS()
+class AGAIN30_API UUFootstepAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	FName BoneName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USoundCue> FootStepSound;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	float VolumeMultiplier;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	float CheckDistance;
+
+public:
+	UUFootstepAnimNotify();
+	
+private:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
